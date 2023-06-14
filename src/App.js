18bdd973afa4bf3e0cfa4d7e91e4a042ff318dc7
otmanTR <r.brk.otman@gmail.com>
2023-06-14 12:29:03 +0300

    Display data

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Greeting from "./components/Greeting";
-import { Provider } from "react-redux";
-import store from "./redux/store";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import Greeting from './components/Greeting';
+import store from './redux/store';
 
- function App() {
+function App() {
   return (
     <Provider store={store}>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Greeting />} />
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Greeting />} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
   );
 }
